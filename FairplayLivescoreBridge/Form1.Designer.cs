c8fc437d689ece7575a4653c8abef0ac35a47b54
Joshua Carroll <joshcarroll21@gmail.com>
2020-12-08 16:07:43 -0600

    Incorporated new classes

--- FairplayLivescoreBridge/Form1.Designer.cs
+++ FairplayLivescoreBridge/Form1.Designer.cs
@@ -29,85 +29,63 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.txtPort = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.btnListen = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.txtResponse = new System.Windows.Forms.TextBox();
             this.txtComRcvd = new System.Windows.Forms.TextBox();
             this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtOutput = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
-            // 
-            // txtPort
-            // 
-            this.txtPort.Location = new System.Drawing.Point(205, 12);
-            this.txtPort.Name = "txtPort";
-            this.txtPort.Size = new System.Drawing.Size(100, 31);
-            this.txtPort.TabIndex = 0;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 15);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(187, 25);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Port to receive on:";
-            // 
-            // btnListen
-            // 
-            this.btnListen.Location = new System.Drawing.Point(323, 10);
-            this.btnListen.Name = "btnListen";
-            this.btnListen.Size = new System.Drawing.Size(117, 35);
-            this.btnListen.TabIndex = 2;
-            this.btnListen.Text = "Listen";
-            this.btnListen.UseVisualStyleBackColor = true;
-            this.btnListen.Click += new System.EventHandler(this.btnListen_Click);
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(455, 10);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(117, 35);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "Stop";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // txtResponse
-            // 
-            this.txtResponse.Location = new System.Drawing.Point(12, 68);
-            this.txtResponse.Multiline = true;
-            this.txtResponse.Name = "txtResponse";
-            this.txtResponse.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.txtResponse.Size = new System.Drawing.Size(225, 677);
-            this.txtResponse.TabIndex = 5;
             // 
             // txtComRcvd
             // 
-            this.txtComRcvd.Location = new System.Drawing.Point(256, 68);
+            this.txtComRcvd.Location = new System.Drawing.Point(12, 108);
             this.txtComRcvd.Multiline = true;
             this.txtComRcvd.Name = "txtComRcvd";
-            this.txtComRcvd.Size = new System.Drawing.Size(524, 677);
+            this.txtComRcvd.Size = new System.Drawing.Size(524, 637);
             this.txtComRcvd.TabIndex = 6;
             // 
             // serialPort1
             // 
             this.serialPort1.PortName = "COM3";
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 25);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Input";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(542, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 25);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Output";
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(542, 108);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(524, 637);
+            this.txtOutput.TabIndex = 8;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1489, 757);
+            this.ClientSize = new System.Drawing.Size(1087, 757);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.txtComRcvd);
-            this.Controls.Add(this.txtResponse);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.btnListen);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.txtPort);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -115,14 +93,11 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox txtPort;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button btnListen;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox txtResponse;
         private System.Windows.Forms.TextBox txtComRcvd;
         private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtOutput;
     }
 }
 
--- FairplayLivescoreBridge/Form1.cs
+++ FairplayLivescoreBridge/Form1.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,17 +16,18 @@ namespace FairplayLivescoreBridge
 {
     public partial class Form1 : Form
     {
-        private TcpListener listener;
-        bool keepListening = false;
         HeavyTask hvtask;
+        Fairplay.Mp70Rs232Data data;
+        ScoreboardOCRData scoreboardOCRData;
 
         public Form1()
         {
             InitializeComponent();
         }
-
         private void Form1_Load(object sender, EventArgs e)
         {
+            data = new Fairplay.Mp70Rs232Data();
+            scoreboardOCRData = new ScoreboardOCRData();
             serialPort1.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
             serialPort1.Open();
         }
@@ -33,40 +35,36 @@ namespace FairplayLivescoreBridge
         private void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e) {
             SerialPort sp = (SerialPort)sender;
             string indata = sp.ReadExisting();
-            txtComRcvd.Text = indata;
+            txtComRcvd.Invoke(new ComReceiverDelegate(ComReceiver), indata);
+        }
+        private delegate void ComReceiverDelegate(string i);
+        private void ComReceiver(string input)
+        {
+            data.Receive(input);
+            write();
+            write("Game clock", data.GameClock);
+            write("Shot clock ",data.ShotClock);
+            write("Game period", data.GamePeriod);
+            write("Home team", data.Home.TeamName);
+            write("Home score", data.Home.Score.ToString());
+            write("Away team", data.Away.TeamName);
+            write("Away score", data.Away.Score.ToString());
 
-            //callmyfunction(indata);
+            txtOutput.Text = scoreboardOCRData.Parse(data).ToJson();
         }
 
-        private void btnListen_Click(object sender, EventArgs e)
+        private void write()
         {
-            keepListening = true;
-
-            hvtask = new HeavyTask();
-            hvtask.Callback1 += CallbackChangeFirstButton;
-            hvtask.Start();
+            txtComRcvd.Text = "";
+        }
+        private void write(string label, string value)
+        {
+            txtComRcvd.Text += string.Format("{0}: {1}{2}",label,value, Environment.NewLine);
         }
 
-        private void CallbackChangeFirstButton(object sender, HeavyTaskResponse response)
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
-            Receive(response.Message);
-        }
-
-        private void Receive(string strReceived, bool newLine = true)
-        {
-            if ((newLine) && (!strReceived.EndsWith(Environment.NewLine)))
-            {
-                strReceived += Environment.NewLine;
-            }
-
-            Console.WriteLine(strReceived);
-            txtResponse.Text += strReceived;
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            keepListening = false;
-            hvtask.Stop();
+            serialPort1.Close();
         }
     }
 }
