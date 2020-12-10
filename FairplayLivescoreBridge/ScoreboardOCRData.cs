1ebfb289dc2bf425d96856de637494ad4cd9d575
Joshua Carroll <joshcarroll21@gmail.com>
2020-12-10 12:57:24 -0600

    Added getters and setters to ensure data integrity

--- FairplayLivescoreBridge/ScoreboardOCRData.cs
+++ FairplayLivescoreBridge/ScoreboardOCRData.cs
@@ -2,6 +2,7 @@
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,15 +11,100 @@ namespace FairplayLivescoreBridge
 {
     class ScoreboardOCRData
     {
-        public string ScoreAway;
-        public string ScoreHome;
-        public string Period;
         public string GameClock;
-        public string ShotClock;
-        public string FoulsAway;
-        public string FoulsHome;
-        public string TimeoutsAway;
-        public string TimeoutsHome;
+        public string Period;
+        public string ShotClock
+        {
+            get
+            {
+                return shotClock;
+            }
+            set
+            {
+                if (value == null)
+                {
+                    shotClock = "";
+                }
+                else
+                {
+                    shotClock = value.Trim();
+                }
+            }
+        }
+        private string ScoreAway
+        {
+            get
+            {
+                return scoreAway;
+            }
+            set
+            {
+                scoreAway = checkNumericValue(value);
+            }
+        }
+        private string ScoreHome
+        {
+            get
+            {
+                return scoreHome;
+            }
+            set
+            {
+                scoreHome = checkNumericValue(value);
+            }
+        }
+        public string FoulsAway
+        {
+            get
+            {
+                return foulsAway;
+            }
+            set
+            {
+                foulsAway = checkNumericValue(value);
+            }
+        }
+        private string FoulsHome
+        {
+            get
+            {
+                return foulsHome;
+            }
+            set
+            {
+                foulsHome = checkNumericValue(value);
+            }
+        }
+        private string TimeoutsAway
+        {
+            get
+            {
+                return timeoutsAway;
+            }
+            set
+            {
+                timeoutsAway = checkNumericValue(value);
+            }
+        }
+        private string TimeoutsHome
+        {
+            get
+            {
+                return timeoutsHome;
+            }
+            set
+            {
+                timeoutsHome = checkNumericValue(value);
+            }
+        }
+
+        private string scoreAway;
+        private string scoreHome;
+        private string foulsAway;
+        private string foulsHome;
+        private string timeoutsAway;
+        private string timeoutsHome;
+        private string shotClock;
 
         public ScoreboardOCRData Parse (Mp70Rs232Data mp70Rs232Data)
         {
@@ -37,8 +123,26 @@ namespace FairplayLivescoreBridge
 
         public string ToJson()
         {
-            string json = JsonConvert.SerializeObject(this, Formatting.Indented);
-            return "{\"type\":\"ocr\",\"values\":\"" + json + "}";
+            string json = JsonConvert.SerializeObject(this, Formatting.None);
+            json = "{\"type\":\"ocr\",\"values\":" + json + "}\n";
+            Debug.WriteLine(json);
+            return json;
+        }
+
+        private string checkNumericValue(string val)
+        {
+            int i;
+
+            if ((val == null) || (val.Trim() == ""))
+            {
+                val = "0";
+            }
+            else if (!int.TryParse(val, out i))
+            {
+                val = "0";
+            }
+
+            return val;
         }
     }
 }
